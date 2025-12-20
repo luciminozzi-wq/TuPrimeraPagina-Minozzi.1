ba307de357ec521bee520cedb327e83795ba13ff
Lucila Minozzi <lulyminozzi@gmail.com>
2025-12-20 13:34:30 -0300

    actualización de la gestión de pacientes en el panel de administración

--- LaboratorioMinozzi/admin.py
+++ LaboratorioMinozzi/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Pacientes, EstudiosDisponibles, ResultadosdeEstudios
 
-# Register your models here.
+@admin.register(Pacientes)
+class PacienteAdmin(admin.ModelAdmin):
+    list_display = ('apellido', 'nombre', 'DNI', 'email')
+    search_fields = ('apellido', 'DNI')
+
+@admin.register(EstudiosDisponibles)
+class EstudioAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'precio')
+
+@admin.register(ResultadosdeEstudios)
+class ResultadoAdmin(admin.ModelAdmin):
+    list_display = ('paciente', 'estudio', 'fecha_estudio', 'bioquimico_responsable')
